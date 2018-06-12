4c03da636350c4688860dca3e57cddf196eb3d83
Konstantin Käfer <mail@kkaefer.com>
2018-06-12 15:58:30 +0200

    prefix backport branch name with target branch name

--- src/backport.js
+++ src/backport.js
@@ -16,17 +16,17 @@ module.exports = async function ({change, branch}) {
     const prPatch = (await octokit.pullRequests.get({owner, repo, number, headers: {accept: 'application/vnd.github.v3.patch'}})).data;
 
     const repository = await git.Repository.open('.');
-    await repository.createBranch(`backport-${number}`, await repository.getBranchCommit(branch));
-    await repository.checkoutBranch(`backport-${number}`);
+    await repository.createBranch(`${branch}__backport-${number}`, await repository.getBranchCommit(branch));
+    await repository.checkoutBranch(`${branch}__backport-${number}`);
 
     execSync(`git am`, {input: prPatch});
-    execSync(`git push origin --set-upstream backport-${number}`);
+    execSync(`git push origin --set-upstream ${branch}__backport-${number}`);
 
     const result = (await octokit.pullRequests.create({
         owner,
         repo,
         title: `${branch} backport: ${prData.title}`,
-        head: `backport-${number}`,
+        head: `${branch}__backport-${number}`,
         base: branch,
         body: `Backports #${number} to ${branch}\n----\n${prData.body}`
     })).data;
